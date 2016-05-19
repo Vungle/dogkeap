6fe805dc02d08eebac9e2d427815d6674a77fa65
feelobot <felix.a.rod@gmail.com>
2016-05-19 13:41:07 -0700

    fix run command

--- run.sh
+++ run.sh
@@ -5,10 +5,10 @@ CONFIG_FILE="/config/telegraf.toml"
 
 echo "=> Starting Telegraf ..."
 
-if [ -z "$DD_API_KEY" ]; then sed -i 's@DD_API_KEY@'$DD_API_KEY'@' $CONFIG_FILE ; fi
-if [ -z "$KAFKA_TOPIC" ]; then sed -i 's@KAFKA_TOPIC@'$KAFKA_TOPIC'@' $CONFIG_FILE ; fi
-if [ -z "$ZK_HOST" ]; then sed -i 's@ZK_HOST@'$ZK_HOST'@' $CONFIG_FILE ; fi
-if [ -z "$CONSUMER_GROUP" ]; then sed -i 's@CONSUMER_GROUP@'$CONSUMER_GROUP'@' $CONFIG_FILE ; fi
-if [ -z "$CONSUMER_OFFSET" ]; then sed -i 's@CONSUMER_OFFSET@'$CONSUMER_OFFSET'@' $CONFIG_FILE ; fi
+sed -i 's@DD_API_KEY@'$DD_API_KEY'@' $CONFIG_FILE
+sed -i 's@KAFKA_TOPIC@'$KAFKA_TOPIC'@' $CONFIG_FILE
+sed -i 's@ZK_HOST@'$ZK_HOST'@' $CONFIG_FILE
+sed -i 's@CONSUMER_GROUP@'$CONSUMER_GROUP'@' $CONFIG_FILE
+sed -i 's@CONSUMER_OFFSET@'$CONSUMER_OFFSET'@' $CONFIG_FILE
 
 exec telegraf -config ${CONFIG_FILE}
